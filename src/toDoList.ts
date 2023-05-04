53055200fe9fa9975fcc24a6fe32c6309789a562
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-05-04 07:26:17 +0300

    modify toDoList

--- src/toDoList.ts
+++ src/toDoList.ts
@@ -1,24 +1,31 @@
 import { TodoItem } from "./toDoItem";
 export class TodoList {
-  private nextId: number = 1; 
-  
-  constructor(public userName: string, public todoItems: TodoItem[] = []) {
+  private nextId: number = 1;
+  private itemMap = new Map<number, TodoItem>();
+  constructor(public userName: string, todoItems: TodoItem[] = []) {
+    todoItems.forEach((item) => this.itemMap.set(item.id, item));
   }
   addTodo(task: string): number {
     while (this.getTodoById(this.nextId)) {
       this.nextId++;
     }
-    this.todoItems.push(new TodoItem(this.nextId, task));
-      return this.nextId;
-    }
-    getTodoById(id: number): TodoItem {
-      return this.todoItems.find(item => item.id === id);
-    }
-    
-    markComplete(id: number, complete: boolean) {
-      const todoItem = this.getTodoById(id);
-      if (todoItem) {
-        todoItem.complete = complete;
-      }
+    this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
+    return this.nextId;
+  }
+  getTodoById(id: number): TodoItem {
+    return this.itemMap.get(id);
+  }
+
+  getTodoItems(includeComplete: boolean): TodoItem[] {
+    return [...this.itemMap.values()].filter(
+      (item) => includeComplete || !item.complete
+    );
+  }
+
+  markComplete(id: number, complete: boolean) {
+    const todoItem = this.getTodoById(id);
+    if (todoItem) {
+      todoItem.complete = complete;
     }
   }
+}
